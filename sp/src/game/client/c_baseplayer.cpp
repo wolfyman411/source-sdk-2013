146df3c961b0cc7c36857045792cea756a630ed7
Peter Covington <petercov@outlook.com>
2023-08-02 16:48:00 -0400

    Also suppress attack3 in vgui screen mode

--- sp/src/game/client/c_baseplayer.cpp
+++ sp/src/game/client/c_baseplayer.cpp
@@ -1146,7 +1146,8 @@ void C_BasePlayer::DetermineVguiInputMode( CUserCmd *pCmd )
 		// Kill all attack inputs if we're in vgui screen mode
 		pCmd->buttons &= ~(IN_ATTACK | IN_ATTACK2);
 #ifdef MAPBASE
-		pCmd->buttons = (pCmd->buttons & ~IN_USE) | IN_VGUIMODE;
+		pCmd->buttons &= ~(IN_USE | IN_ATTACK3);
+		pCmd->buttons |= IN_VGUIMODE;
 #endif // MAPBASE
 		return;
 	}
@@ -1213,7 +1214,8 @@ void C_BasePlayer::DetermineVguiInputMode( CUserCmd *pCmd )
 		// Kill all attack inputs if we're in vgui screen mode
 		pCmd->buttons &= ~(IN_ATTACK | IN_ATTACK2);
 #ifdef MAPBASE
-		pCmd->buttons = (pCmd->buttons & ~IN_USE) | IN_VGUIMODE;
+		pCmd->buttons &= ~(IN_USE | IN_ATTACK3);
+		pCmd->buttons |= IN_VGUIMODE;
 #endif // MAPBASE
 	}
 }
