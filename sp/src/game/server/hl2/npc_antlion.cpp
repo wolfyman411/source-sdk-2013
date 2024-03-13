e954f00f79ee91a3353d388b73d9b5748e7ce2b6
ALLEN-PC\acj30 <acj3017@gmail.com>
2024-03-13 14:00:21 -0500

    Fix antlions using wrong angles for dynamic interactions

--- sp/src/game/server/hl2/npc_antlion.cpp
+++ sp/src/game/server/hl2/npc_antlion.cpp
@@ -378,13 +378,14 @@ void CNPC_Antlion::Spawn( void )
 
 		sInteraction01.vecRelativeOrigin = Vector(224, 0, 0);
 		sInteraction01.angRelativeAngles = QAngle(0, 180, 0);
-		//sInteraction01.iFlags |= SCNPC_FLAG_TEST_OTHER_ANGLES;
+		sInteraction01.iFlags |= SCNPC_FLAG_TEST_OTHER_ANGLES;
 		sInteraction01.iFlags |= SCNPC_FLAG_TEST_END_POSITION;
 		sInteraction01.vecRelativeEndPos = Vector(312, -10, 0);
 		sInteraction01.iTriggerMethod = SNPCINT_AUTOMATIC_IN_COMBAT;
 		sInteraction01.flDelay = 15.0f;
 		sInteraction01.iFlags |= SCNPC_FLAG_MAPBASE_ADDITION;
 		sInteraction01.flDistSqr = (8 * 8);
+		sInteraction01.flMaxAngleDiff = 180.0f; // Initiate from any angle
 
 
 		ScriptedNPCInteraction_t sInteraction02;
@@ -393,11 +394,12 @@ void CNPC_Antlion::Spawn( void )
 
 		sInteraction02.vecRelativeOrigin = Vector(64, 0, 0);
 		sInteraction02.angRelativeAngles = QAngle(0, 180, 0);
-		//sInteraction01.iFlags |= SCNPC_FLAG_TEST_OTHER_ANGLES;
+		sInteraction02.iFlags |= SCNPC_FLAG_TEST_OTHER_ANGLES;
 		sInteraction02.iTriggerMethod = SNPCINT_AUTOMATIC_IN_COMBAT;
 		sInteraction02.flDelay = 7.5f;
 		sInteraction02.iFlags |= SCNPC_FLAG_MAPBASE_ADDITION;
 		sInteraction02.flDistSqr = (8 * 8);
+		sInteraction02.flMaxAngleDiff = 180.0f; // Initiate from any angle
 
 
 		AddScriptedNPCInteraction(&sInteraction01);
